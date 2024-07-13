7a6e955454a4802d7bf8dd964f5b8e341d17077c
AdrianOlivaV <jao.vejar@gmail.com>
2024-07-13 12:25:49 -0700

    pequeño proyecto de practica

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String nombre="pedro";
+        String tipoDeCuenta="Corriente";
+        double saldo = 1599;
+
+        System.out.println("************************************");
+        System.out.println("");
+        }
+    }
+}
+
